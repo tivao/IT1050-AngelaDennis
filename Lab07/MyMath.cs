19ffaccb44211354c1adc2757999e39b5ae30a60
tivao <34356851+tivao@users.noreply.github.com>
2018-03-14 21:16:42 -0400

    Add files via upload

---
+++ Lab07/MyMath.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab07
+{
+    class MyMath
+    {
+        public double result { get; set; }
+        public double operand1 { get; set; }
+        public double operand2 { get; set; }
+        public void Multiply(double operand1, double operand2)
+        {
+            result = operand1 * operand2;
+        }
+        public void Divide(double operand1, double operand2)
+        {
+            result = operand1 / operand2;
+        }
+        public void Subtract(double operand1, double operand2)
+        {
+            result = operand1 - operand2;
+        }
+        public void Add(double operand1, double operand2)
+        {
+            result = operand1 + operand2;
+        }
+        public double GetResult { get; set; }
+    }
+}
